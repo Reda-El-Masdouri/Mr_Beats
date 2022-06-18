263eaaf311901d29adb869ecc437517626d54225
Reda E <reda.elmasdouri@gmail.com>
2022-06-18 22:58:32 +0200

    Création du projet, widget track, nombre de track dynamique et sound kit service

---
+++ main.py
@@ -0,0 +1,32 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.widget import Widget
+
+from Mr_Beats.sound_kit_service import SoundKitService
+from Mr_Beats.track import TrackWidget
+
+Builder.load_file("track.kv")
+
+
+class MainWidget(RelativeLayout):
+    tracks_layout = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+        self.sound_kit_service = SoundKitService()
+    def on_parent(self, widget, parent):
+        for i in range(0, self.sound_kit_service.get_nb_tracks()):
+            self.tracks_layout.add_widget(TrackWidget())
+
+
+
+
+
+class MrBeatApp(App):
+    pass
+
+
+MrBeatApp().run()
