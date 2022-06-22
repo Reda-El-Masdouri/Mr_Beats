9d8c0206ffff67bf4702c85f2cfbad304ba90639
Reda E <reda.elmasdouri@gmail.com>
2022-06-22 22:29:38 +0200

    Jouer les sons

--- audio_engine.py
+++ audio_engine.py
@@ -7,10 +7,12 @@ class AudioEngine:
     NB_CHANELS = 1
     SAMPLE_RATE = 44100
     BUFFER_SIZE = 1024
+
     def __init__(self):
         self.output_stream = get_output(channels=self.NB_CHANELS, rate=self.SAMPLE_RATE, buffersize=self.BUFFER_SIZE)
 
+        self.audio_source_one_shot = AudioSourceOneShot(self.output_stream)
+        self.audio_source_one_shot.start()
 
-    def ply_sound(self, wave_samples):
-        audio_source = AudioSourceOneShot(self.output_stream, wave_samples)
-        audio_source.start()
+    def play_sound(self, wav_samples):
+        self.audio_source_one_shot.set_wav_samples(wav_samples)
